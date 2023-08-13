d921efcdeb8c92419cf67b9a256ca9c2b65cf1a9
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-13 20:33:15 +0300

    added character list endpoint with filtering by name functionality

--- characters/urls.py
+++ characters/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from characters.views import get_random_character_view
+from characters.views import get_random_character_view, CharacterListView
 
 urlpatterns = [
+    path("characters/", CharacterListView.as_view(), name="character-list"),
     path("characters/random/", get_random_character_view, name="character-random"),
 ]
 
